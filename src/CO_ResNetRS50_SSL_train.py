12a0a19737580b7d508709de29d10accf8252d89
Yang <853580099@qq.com>
2024-10-06 22:31:51 +0800

    xiugaidaima

---
+++ src/CO_ResNetRS50_SSL_train.py
@@ -0,0 +1,230 @@
+import sys
+import os
+import torch
+import torch.nn as nn
+import argparse
+import torch.optim as optim
+import numpy as np
+from tqdm import tqdm
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from model.CO_ResNetRS50_SSL.ResNetRS import ResNet
+from preprocess import load_data
+from torch.optim.lr_scheduler import _LRScheduler
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import precision_score, recall_score, f1_score
+import pandas as pd
+from torch.utils.data import DataLoader
+
+use_cuda = torch.cuda.is_available()
+device = torch.device("cuda" if use_cuda else "cpu")
+print(device)
+
+def get_args():
+    parser = argparse.ArgumentParser('parameters')
+    parser.add_argument('--epochs', type=int, default=80, help='number of epochs, (default: 80)')
+    parser.add_argument('--num_classes', type=int, default=7, help='number of num_classes, (default: 7)')
+    parser.add_argument('--learning-rate', type=float, default=0.01, help='learning rate, (default: 1e-1)')
+    parser.add_argument('--batch-size', type=int, default=32, help='batch size, (default: 32)')
+    parser.add_argument('--input-size', type=tuple, default=(224, 224), help='input data size, (default: (224, 224))')
+    parser.add_argument('--confidence-threshold', type=float, default=0.90, help='Confidence threshold for pseudo-labeling.')
+    args = parser.parse_args()
+    return args
+
+
+class WarmUpLR(_LRScheduler):
+    def __init__(self, optimizer, total_iters, last_epoch=-1):
+        self.total_iters = total_iters
+        super().__init__(optimizer, last_epoch)
+    
+    def get_lr(self):
+        return [base_lr * self.last_epoch / (self.total_iters + 1e-8) for base_lr in self.base_lrs]
+
+def adjust_learning_rate(optimizer, epoch, args):
+    lr = args.learning_rate * (0.1 ** (epoch // 30))
+    for param_group in optimizer.param_groups:
+        param_group['lr'] = lr
+
+def count_parameters(model):
+    return sum(p.numel() for p in model.parameters())
+
+def main(args):
+    train_loader, val_loader, test_loader, unlabeled_loader = load_data(args)
+
+    original_train_df = train_loader.dataset.dataframe.copy()
+    original_class_counts = original_train_df['growth_stage_code'].value_counts().sort_index()
+    label_mapping = {0: 1, 1: 2, 2: 3, 3: 4, 4: 5, 5: 7, 6: 8}
+
+    model = ResNet.build_model("resnetrs50").to(device)
+
+    optimizer = optim.SGD(model.parameters(), lr=args.learning_rate, weight_decay=5e-4, momentum=0.9)
+    criterion = nn.CrossEntropyLoss().to(device)
+
+    print(f"Total parameters in the model: {count_parameters(model):,}")
+ 
+    best_val_accuracy = 0.0
+    # 新增：创建CSV文件并写入列标题
+    with open("./data/CO_ResNetRS50_SSL/result.csv", "w") as f:
+        f.write("Epoch,Number_of_Unlabeled_Images,Train_Loss,Train_Acc,Val_Loss,Val_Acc,Test_Loss,Test_Acc,Test_Pre,Test_Recall,Test_F1,Test_1_Acc,Test_2_Acc,Test_3_Acc,Test_4_Acc,Test_5_Acc,Test_7_Acc,Test_8_Acc,Test_Confusion_Matrix\n")
+
+        total_difference = None        
+        for epoch in range(1, args.epochs + 1):
+            model.train()
+            train_loss = 0
+            train_correct = 0
+            processed_data = 0
+            progress_bar = tqdm(train_loader, desc="Training", mininterval=1)
+            for batch_idx, (data, target) in enumerate(progress_bar):
+                adjust_learning_rate(optimizer, epoch, args)
+                data, target = data.to(device), target.to(device)
+                optimizer.zero_grad()
+                output = model(data)
+                loss = criterion(output, target)
+                loss.backward()
+                optimizer.step()
+                train_loss += loss.item() * data.size(0)
+                y_pred = output.argmax(dim=1, keepdim=True)
+                train_correct += y_pred.eq(target.view_as(y_pred)).sum().item()
+                processed_data += data.size(0)
+                average_train_loss = train_loss / processed_data
+                average_train_accuracy = train_correct / processed_data
+                progress_bar.set_description(f"Epoch {epoch:02d}, Loss: {average_train_loss:.4f}, Acc: {average_train_accuracy:.4f}, LR: {optimizer.param_groups[0]['lr']:.6f},")
+
+
+            # 验证模型
+            model.eval()
+            val_loss = 0
+            val_correct = 0
+            processed_data = 0
+            all_preds = []
+            all_targets = []
+            progress_bar = tqdm(val_loader, desc="Validation", mininterval=1)
+            with torch.no_grad():
+                for data, target in progress_bar:
+                    data, target = data.to(device), target.to(device)
+                    output = model(data)
+                    loss = criterion(output, target)
+                    val_loss += loss.item() * data.size(0)
+                    pred = output.argmax(dim=1, keepdim=True)
+                    val_correct += pred.eq(target.view_as(pred)).sum().item()
+                    processed_data += data.size(0)
+                    all_preds.extend(pred.cpu().numpy())
+                    all_targets.extend(target.cpu().numpy())
+                    average_val_loss = val_loss / processed_data
+                    average_val_accuracy = val_correct / processed_data
+                    progress_bar.set_description(f"Val Loss: {average_val_loss:.4f}, Val Accuracy: {average_val_accuracy:.4f}")
+
+            conf_matrix = confusion_matrix(all_targets, all_preds)
+            print("Confusion Matrix:")
+            print(conf_matrix)
+
+
+            # 将结果写入CSV文件
+            if epoch < args.epochs:
+                if args.ssl==False:
+                    f.write(f"{epoch},{average_train_loss:.4f},{average_train_accuracy:.4f},{average_val_loss:.4f},{average_val_accuracy:.4f},{None},{None},{None},{None},{None},{None},{None},{None},{None},{None},{None},{None},{None}\n")
+                else:
+                    f.write(f"{epoch},{total_difference},{average_train_loss:.4f},{average_train_accuracy:.4f},{average_val_loss:.4f},{average_val_accuracy:.4f},{None},{None},{None},{None},{None},{None},{None},{None},{None},{None},{None},{None},{None}\n")
+
+            if best_val_accuracy < average_val_accuracy:
+                print('Saving..')
+                state = {
+                    'model': model.state_dict(),
+                    'loss': val_loss,
+                    'acc': average_val_accuracy,
+                    'epoch': epoch,
+                }
+                filename = "Best_model_"
+                torch.save(state, './model/CO_ResNetRS50_SSL/' + filename + 'ckpt.t7')
+                best_val_accuracy = average_val_accuracy
+
+
+
+            if epoch < args.epochs:
+                # 恢复最初的训练集，并添加伪标签数据
+                train_loader.dataset.dataframe = original_train_df.copy()
+                model.eval()
+                all_preds = []
+                all_image_uuids = []
+                with torch.no_grad():
+                    for data, image_uuids in unlabeled_loader:
+                        data = data.to(device)
+                        output = model(data)
+                        probs = torch.softmax(output, dim=1)
+                        max_probs, preds = torch.max(probs, dim=1)
+                        for i in range(len(image_uuids)):
+                            if max_probs[i].item() >= args.confidence_threshold:
+                                mapped_label = label_mapping.get(preds[i].item(), preds[i].item())
+                                all_preds.append(mapped_label)
+                                all_image_uuids.append(image_uuids[i])
+                # 将伪标签加入训练集
+                if all_image_uuids:
+                    pseudo_df = pd.DataFrame({
+                        'image_uuid': all_image_uuids, 
+                        'growth_stage_code': all_preds
+                    })
+                    # 加载训练集的数据框，并合并伪标签数据
+                    # train_df = train_loader.dataset.dataframe
+                    updated_train_df = pd.concat([original_train_df, pseudo_df], ignore_index=True)
+                    # 创建新的数据集
+                    train_loader.dataset.dataframe = updated_train_df
+                    train_loader = DataLoader(train_loader.dataset, batch_size=args.batch_size, shuffle=True, drop_last=True)
+                # 计算当前训练集中的类别分布
+                current_class_counts = train_loader.dataset.dataframe['growth_stage_code'].value_counts().sort_index()
+                # 计算类别数目差异
+                class_difference = current_class_counts - original_class_counts
+                total_difference = class_difference.sum()
+                for class_label, diff in class_difference.items():
+                    print(f"Epoch {epoch+1:02d}, Class {class_label} Difference: {diff}")
+                print(f"Epoch {epoch+1:02d}, Number of Unlabeled Images: {total_difference}")
+            
+
+        # 测试模型
+        val_checkpoint = torch.load('./model/CO_ResNetRS50_SSL/Best_model_ckpt.t7')
+        model.load_state_dict(val_checkpoint['model'])
+        val_epoch = val_checkpoint['epoch']
+        val_acc = val_checkpoint['acc']
+        print("Load Validation Model Accuracy: ", val_acc, "epoch: ", val_epoch)
+        model.eval()
+        test_loss = 0
+        test_correct = 0
+        processed_data = 0
+        all_preds = []
+        all_targets = []
+        progress_bar = tqdm(test_loader, desc="Test", mininterval=1)
+        with torch.no_grad():
+            for data, target in progress_bar:
+                data, target = data.to(device), target.to(device)
+                output = model(data)
+                loss = criterion(output, target)
+                test_loss += loss.item() * data.size(0)
+                pred = output.argmax(dim=1, keepdim=True)
+                test_correct += pred.eq(target.view_as(pred)).sum().item()
+                processed_data += data.size(0)
+                all_preds.extend(pred.cpu().numpy())
+                all_targets.extend(target.cpu().numpy())
+                average_test_loss = test_loss / processed_data
+                average_test_accuracy = test_correct / processed_data
+                progress_bar.set_description(f"Loss: {average_test_loss:.4f}, Acc: {average_test_accuracy:.4f}")
+
+        conf_matrix = confusion_matrix(all_targets, all_preds)
+        print("Confusion Matrix:")
+        print(conf_matrix)
+
+        # 计算Precision、Recall、F1 Score
+        average_test_precision = precision_score(all_targets, all_preds, average='macro')
+        average_test_recall = recall_score(all_targets, all_preds, average='macro')
+        average_test_f1 = f1_score(all_targets, all_preds, average='macro')
+        print(f"Test Precision: {average_test_precision:.4f}, Test Recall: {average_test_recall:.4f}, Test F1 Score: {average_test_f1:.4f}")
+        # 计算每个类别的准确率
+        accuracy_per_class = conf_matrix.diagonal() / conf_matrix.sum(axis=1)
+        for i in range(len(accuracy_per_class)):
+            print(f"Class {i}: Accuracy: {accuracy_per_class[i]:.4f}")
+
+        # 将结果写入CSV文件
+        conf_matrix_str = np.array_str(conf_matrix).replace('\n', ' ')
+        f.write(f"{epoch},{total_difference},{average_train_loss:.4f},{average_train_accuracy:.4f},{average_val_loss:.4f},{average_val_accuracy:.4f},{average_test_loss:.4f},{average_test_accuracy:.4f},{average_test_precision:.4f},{average_test_recall:.4f},{average_test_f1:.4f},{accuracy_per_class[0]:.4f},{accuracy_per_class[1]:.4f},{accuracy_per_class[2]:.4f},{accuracy_per_class[3]:.4f},{accuracy_per_class[4]:.4f},{accuracy_per_class[5]:.4f},{accuracy_per_class[6]:.4f},{conf_matrix_str}\n")
+
+
+if __name__ == '__main__':
+    args = get_args()
+    main(args)
--- src/preprocess.py
+++ src/preprocess.py
@@ -1,10 +1,11 @@
+import os
 import torch
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from torchvision import transforms
 from torch.utils.data import DataLoader
 from torchvision.datasets.folder import default_loader
-import os
+
 
 class CustomDataset(torch.utils.data.Dataset):
     def __init__(self, dataframe, transform=None):
@@ -15,12 +16,11 @@ class CustomDataset(torch.utils.data.Dataset):
         return len(self.dataframe)
 
     def __getitem__(self, idx):
-        img_name = os.path.join('../../Rice2/gs_img/', self.dataframe.iloc[idx, 0] + '.jpeg')
+        img_name = os.path.join('./RiceImagesData/', self.dataframe.iloc[idx, 0] + '.jpeg')
         image = default_loader(img_name)
 
         label = int(self.dataframe.iloc[idx, 1])
-        label_mapping = {10: 0, 20: 1, 30: 2, 40: 3, 50: 4, 70: 5, 80: 6}
-        # label_mapping = {10: 0, 11: 1, 12: 2, 13: 3, 14: 4, 15: 5, 16: 6, 17: 7, 18: 8, 19: 9, 21: 10, 22: 11, 23: 12, 24: 13, 25: 14, 26: 15, 27: 16, 28: 17, 29: 18, 30: 19, 32: 20, 34: 21, 37: 22, 39: 23, 41: 24, 43: 25, 45: 26, 47: 27, 49: 28, 51: 29, 52: 30, 53: 31, 54: 32, 55: 33, 56: 34, 57: 35, 58: 36, 59: 37, 71: 38, 73: 39, 75: 40, 77: 41, 83: 42, 85: 43, 87: 44, 89: 45}
+        label_mapping = {1: 0, 2: 1, 3: 2, 4: 3, 5: 4, 7: 5, 8: 6}
         label = label_mapping[label]
 
         if self.transform:
@@ -38,13 +38,11 @@ class UnlableDataset(torch.utils.data.Dataset):
         return len(self.dataframe)
 
     def __getitem__(self, idx):
-        img_name = os.path.join('../../Rice2/gs_img/', self.dataframe.iloc[idx, 0] + '.jpeg')
+        img_name = os.path.join('./RiceImagesData/', self.dataframe.iloc[idx, 0] + '.jpeg')
         image = default_loader(img_name)
         uuid = self.dataframe.iloc[idx, 0]  # 获取uuid
-
         if self.transform:
             image = self.transform(image)
-
         return image, uuid
 
 def count_labels(dataset):
@@ -56,17 +54,28 @@ def count_labels(dataset):
             label_count[label] = 1
     return label_count
 
+def save_label_count_to_csv(label_count, filename):
+    # 标签映射字典
+    label_mapping = {0: 1, 1: 2, 2: 3, 3: 4, 4: 5, 5: 7, 6: 8}
+    # 使用映射更新标签
+    mapped_label_count = {label_mapping[label]: count for label, count in label_count.items()}
+    # 将字典转换为DataFrame并按标签排序
+    df = pd.DataFrame(list(mapped_label_count.items()), columns=['Label', 'Count'])
+    df.sort_values(by='Label', ascending=True, inplace=True)  # 按标签排序
+    df.to_csv(filename, index=False)
+    print(f"标签统计结果已保存到 {filename}")
+
 def load_data(args):
-    csv_file = './clean_data.csv'
-    df = pd.read_csv(csv_file)
-    # df = pd.read_csv(csv_file, dtype={'image_uuid': str})
+    train_csv_file = './data/train_data.csv'
+    val_csv_file = './data/val_data.csv'
+    test_csv_file = './data/test_data.csv'
+    unlabeled_csv_file = './data/unlabeled_data.csv'
 
-    # 确保每个类别都划分30%数据到无标签数据集中
-    labeled_df, unlabeled_df = train_test_split(df, test_size=0.3, random_state=36, stratify=df['growth_stage_code'])
-    # 将有标签的数据集划分为训练集、验证集和测试集
-    train_df, test_val_df = train_test_split(labeled_df, test_size=0.3, random_state=36, stratify=labeled_df['growth_stage_code'])
-    val_df, test_df = train_test_split(test_val_df, test_size=0.5, random_state=36, stratify=test_val_df['growth_stage_code'])
-
+    # 读取训练集、验证集和测试集数据
+    train_df = pd.read_csv(train_csv_file)
+    val_df = pd.read_csv(val_csv_file)
+    test_df = pd.read_csv(test_csv_file)
+    unlabeled_df = pd.read_csv(unlabeled_csv_file)
 
     transform_train = transforms.Compose([
         transforms.Resize(args.input_size),
@@ -76,8 +85,7 @@ def load_data(args):
         transforms.Normalize((0.5071, 0.4865, 0.4409), (0.2673, 0.2564, 0.2762))
     ])
 
-    transform_unlabeled = transform_train  # 使用相同的数据增强策略
-
+    transform_unlabeled = transform_train
 
     transform_val = transforms.Compose([
         transforms.Resize(args.input_size),
@@ -98,25 +106,21 @@ def load_data(args):
     val_loader = DataLoader(val_dataset, batch_size=args.batch_size, shuffle=False)
     test_loader = DataLoader(test_dataset, batch_size=args.batch_size, shuffle=False)
     unlabeled_loader = DataLoader(unlabeled_dataset, batch_size=args.batch_size, shuffle=False)
-    
 
-
-
-    # 统计训练集中各个标签的图像数量
+    # 统计并保存训练集中各个标签的图像数量
     train_label_count = count_labels(train_loader.dataset)
     print("训练集中各个标签的图像数量：", train_label_count)
+    save_label_count_to_csv(train_label_count, './data/dataset_count/train_label_count.csv')
 
-    # 统计验证集中各个标签的图像数量
+    # 统计并保存验证集中各个标签的图像数量
     val_label_count = count_labels(val_loader.dataset)
     print("验证集中各个标签的图像数量：", val_label_count)
+    save_label_count_to_csv(val_label_count, './data/dataset_count/val_label_count.csv')
 
-    # 统计测试集中各个标签的图像数量
+    # 统计并保存测试集中各个标签的图像数量
     test_label_count = count_labels(test_loader.dataset)
     print("测试集中各个标签的图像数量：", test_label_count)
-
-    # 统计无标签数据集中图像的数量
-    unlabeled_sample_count = len(unlabeled_loader.dataset)
-    print("无标签数据集中图像的数量：", unlabeled_sample_count)
+    save_label_count_to_csv(test_label_count, './data/dataset_count/test_label_count.csv')
 
     return train_loader, val_loader, test_loader, unlabeled_loader
 
